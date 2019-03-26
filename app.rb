0b7162a0dc96941212595ddf42ca886a62508a79
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-26 22:29:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number]
     @phrase = params[:phrase]
-    print @phrase * @number.to_i
+    "#{@phrase * @number.to_i}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
