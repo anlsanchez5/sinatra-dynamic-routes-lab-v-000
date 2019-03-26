7fac6f94d5e0dce7384dd130acf950cd56937133
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-26 22:24:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number]
     @phrase = params[:phrase]
-    @number.times do print @phrase
+    @number.times do print @phrase end
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
