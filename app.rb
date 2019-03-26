103dcbedff139404fef5a6f47544b7655ea02961
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-26 22:18:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   get '/square/:number' do
     @number = params[:number].to_i
-    "#{@number.square!}"
+    "#{@number.to_i_square!}"
   end
 
   get '/say/:number/:phrase' do
