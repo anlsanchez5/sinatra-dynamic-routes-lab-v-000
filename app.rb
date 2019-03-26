6eddfbfff72d528159cfcb542ce6262ec9b8ec74
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-26 22:22:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number]
     @phrase = params[:phrase]
+    print @phrase * @number 
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
